f445e9c03540e37c0ff2c4927490e3a9ff5feb1a
Justine Bayron <Jbayron76@gmail.com>
2023-01-21 22:00:25 +0800

    fileHandling.java that inputs information of 10 student through multidimensional array, write it in the file text and read and display it in Jframe

--- fileHandling.java
+++ fileHandling.java
@@ -2,82 +2,113 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
-
 import javax.swing.*;
-import javax.swing.table.*;
+import javax.swing.table.DefaultTableCellRenderer;
+
 
 public class fileHandling {
     private static String name, birthday, gender;
-    private static int age, contactNum;
+    private static int age, contactNum, person_Count, lineCounter = 0;
+    private static boolean looping = true;
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-
-        // for (int i = 0; i < 1; i++) {
-        //     int counter = i + 1;
-        //     System.out.println("Person #" + counter);
-        //     System.out.println("Enter your name: ");
-        //     name = inputScanner.next();
-        //     System.out.println("Enter your birthday: ");
-        //     birthday = inputScanner.next();
-        //     System.out.println("Gender: ");
-        //     gender = inputScanner.next();
-        //     System.out.println("Enter age: ");
-        //     age = inputScanner.nextInt();
-        //     System.out.println("Contact Number: ");
-        //     contactNum = inputScanner.nextInt();
-        //     System.out.println();
-
-        //     try {
-        //         FileWriter fw = new FileWriter("phonebook.txt", true);
-        //         BufferedWriter bw = new BufferedWriter(fw);
-        //         bw.write(name + " " + age + " " + birthday + " " + gender + " " + age + " " + contactNum + " \n");
-        //         bw.close();
-        //     } catch (IOException e) {
-        //         e.printStackTrace();
-        //     }
-        // }
-        
-        System.out.println("[1]Read File\n[2]Exit");
-        int choice = inputScanner.nextInt();
         String[] columnNames = {"Name", "Birthday", "Gender", "Age", "Contact Number"};
-        
-        if (choice == 1) {
-            Object[][] data = new Object[5][10];
-            try {
-                FileReader fr = new FileReader("phonebook.txt");
-                BufferedReader br = new BufferedReader(fr);
-                String dataValue;
-                
-                int i = 0;
-                while((dataValue = br.readLine()) != null) {
-                    System.out.println(dataValue);
-                    for (int j = 0; j < data.length; j++) {
-                        data[i] = dataValue.split(" ");
+        Object[][] data = new Object[10][5];
+
+
+        while(looping) {
+            System.out.println("[1]ADD PERSON\n[2]READ DATA\n[3]EXIT");
+            int choice = inputScanner.nextInt();
+            switch (choice) {
+                case 1:
+
+                    try {
+                        // To find if there are informations already stored in the files through iterating if the file has next line.
+                        BufferedReader brLine = new BufferedReader(new FileReader("phonebook.txt"));
+                        String textLine = brLine.readLine();
+                        while (textLine != null) {
+                            lineCounter++;
+                            textLine = brLine.readLine();
+                        }
+                        brLine.close();
+                    } catch (Exception e) {
+                        System.err.print("IOException error");
+                        e.printStackTrace();
                     }
-                    i++;
-                }
+                    //Set the lineCounter as the person_Count to start the iteration there. Ask for input to be stored.
+                    for (person_Count = lineCounter; person_Count < data.length; person_Count++) {
+                        int counter = person_Count + 1;
+                        System.out.println("Person #" + counter);
+                        System.out.println("Enter your name: ");
+                        name = inputScanner.next();
+                        System.out.println();
+                        System.out.println("Enter your birthday: ");
+                        System.out.print("Month: ");
+                        birthday = inputScanner.next() + " ";
+                        System.out.print("Day: ");
+                        birthday = birthday + inputScanner.next() + " ";
+                        System.out.print("Year: ");
+                        birthday = birthday + inputScanner.next();
+                        System.out.println();
+                        System.out.println("Gender: ");
+                        gender = inputScanner.next();
+                        System.out.println();
+                        System.out.println("Enter age: ");
+                        age = inputScanner.nextInt();
+                        System.out.println("Contact Number: ");
+                        contactNum = inputScanner.nextInt();
+                        System.out.println();
+                        //Write the information inputted to the file phonebook.txt
+                        try {
+                            FileWriter fw = new FileWriter("phonebook.txt", true);
+                            BufferedWriter bw = new BufferedWriter(fw);
+                            bw.write(name + ", " + birthday + ", " + gender + ", " + age + ", " + contactNum + ", ");
+                            bw.newLine();
+                            bw.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+   
+                    break;
+                case 2:
 
-                br.close();
-    
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader("phonebook.txt"));
+                        String dataValue;
+                        int i = 0;
+                        while((dataValue = br.readLine()) != null) {
+                             System.out.println(dataValue);
+                             if (i < data.length) {
+                                data[i] = dataValue.split(", ");
+                             }
+                             System.out.println(" " + i + " " + data.length);
+                            i++;
+                        }
+                        br.close();
+            
+                    } catch (IOException e) {
+                        System.err.print("IOException error");
+                        e.printStackTrace();
+                    }
+                    JTable table = new JTable(data, columnNames);
+                    JScrollPane JScrollPane = new JScrollPane(table);
+                    JFrame frame = new JFrame("10 students' information.");
+                    frame.add(JScrollPane);
+                    frame.setSize(700, 400);
+                    frame.setVisible(true);
+                    DefaultTableCellRenderer CenterRender = new DefaultTableCellRenderer();
+                    CenterRender.setHorizontalAlignment(JLabel.CENTER);
+                    table.setDefaultRenderer(Object.class, CenterRender);
+                    break;
+                case 3:
+                    looping = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.\n");
+                    break;
             }
-
-            JTable table = new JTable(data, columnNames);
-            JScrollPane JScrollPane = new JScrollPane(table);
-            JFrame frame = new JFrame("10 students' information.");
-            frame.add(JScrollPane);
-            frame.setSize(600, 300);
-            frame.setVisible(true);
         }
-        
-
 
     }
 }
-
-/* Performance task. 
-Create a program that will creata list of contacts of 10 persons. Name your text file with "phonebook.txt" 
-that contains data item to be stored: name, birthday, gender, age, and contact number. The program allows the user to input data 
-and store it in a "phonebook.txt" file, and read the data from the text. */
